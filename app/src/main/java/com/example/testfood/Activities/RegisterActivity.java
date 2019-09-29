dc995526b93f8c7816836645010f68aa580554e1
johnjoma <irungujohnkimani@gmail.com>
2019-09-30 01:18:17 +0300

    FoodGram Application3

---
+++ app/src/main/java/com/example/testfood/Activities/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.example.testfood.Activities;
+
+import android.os.Bundle;
+
+import com.example.testfood.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+}
